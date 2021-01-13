d622f8b90df61db3232f1c38ce5bec28ae03f9c4
Pasca Vlad <pascavlad16@gmail.com>
2021-01-13 17:12:16 +0200

    finished the project
    -made the results visible
    -added the error message

--- app.js
+++ app.js
@@ -3,20 +3,48 @@ document.getElementById("main-form").addEventListener("submit", function (e) {
 
     document.getElementById("loading").style.display = "block";
 
-    setTimeout(calculateTemp, 2000);
+    setTimeout(convert, 2000);
 
     e.preventDefault();
 });
 
-function calculateTemp() {
+function convert() {
     const tempCel = document.getElementById("celsius");
-    const results = document.getElementById("results");
+    const results = document.getElementById("converted");
 
     let calculated = (tempCel.value * 9) / 5 + 32;
 
-    calculated.value = results;
+    if (tempCel.value !== "") {
+        results.value = calculated;
 
-    document.getElementById("results").style.display = "block";
+        document.getElementById("results").style.display = "block";
+
+        document.getElementById("loading").style.display = "none";
+    } else {
+        showError("Make sure your numbers are valid");
+    }
+}
+
+function showError(error) {
+    document.getElementById("results").style.display = "none";
 
     document.getElementById("loading").style.display = "none";
+
+    const errorDiv = document.createElement("div");
+
+    const card = document.querySelector(".card");
+
+    const heading = document.querySelector(".heading");
+
+    errorDiv.className = "alert alert-danger";
+
+    errorDiv.appendChild(document.createTextNode(error));
+
+    card.insertBefore(errorDiv, heading);
+
+    setTimeout(clearError, 3000);
+}
+
+function clearError() {
+    document.querySelector(".alert").remove();
 }
